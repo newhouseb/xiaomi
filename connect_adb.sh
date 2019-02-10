2b29622035337ea5e594341367460d6c0134db04
Ben Newhouse <newhouseb@gmail.com>
2019-02-10 03:04:37 +0000

    I think... this works reliably

--- connect_adb.sh
+++ connect_adb.sh
@@ -6,9 +6,9 @@ fi
 
 adb disconnect 192.168.1.196
 adb connect 192.168.1.196
+sleep 1
 
 if adb devices -l | grep "unauthorized"; then
-  sleep 1
   echo "down" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
   echo "right" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
   echo "enter" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
--- power_on.sh
+++ power_on.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo on 0 | cec-client -s -d 1
+
+n=0
+until [ $n -ge 60 ]
+do
+  bash connect_adb.sh && break
+  n=$[$n+1]
+  sleep 1
+done
+
+
+bash first_input.sh
