c6c0a4cdc88d4e6f27a12e4c2479a6eab9a16729
Ben Newhouse <newhouseb@gmail.com>
2019-02-10 02:45:22 +0000

    Add first input script

---
+++ first_input.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+bash connect_adb.sh
+
+adb shell input keyevent 3 # Go home
+
+echo "esc" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "up" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+
+# Go right a bunch to whiz past any notifications
+echo "right" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "right" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "right" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "right" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "left" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "enter" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+
+# Go up a bunch to get rid of any selection
+echo "up" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "up" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "up" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "up" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+echo "down" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
+sleep 0.1
+
+# Hit enter ont he first thing (i.e. Apple TV)
+echo "enter" | /home/pi/hid_gadget_test /dev/hidg0 keyboard
